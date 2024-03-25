4ed7d662a66d60124a24483bca07445e4e648519
Alazne <150714674+alazne3@users.noreply.github.com>
2024-03-25 18:19:00 +0100

    Add files via upload

---
+++ Checkpoint 8.js
@@ -0,0 +1,31 @@
+var miLista = [
+    'velma',
+    'exploradora',
+    'jane',
+    'john',
+    'harry'
+    ];
+
+//For loop
+
+for (var i = 0; i < miLista.length; i++) {
+    console.log(miLista[i]);
+  }
+
+
+//While loop
+
+var i = 0;
+while (i < miLista.length) {
+  console.log(miLista[i]);
+  i++;
+}
+
+
+//Arrow function
+
+holaMundo = () => {
+    console.log('Hola mundo');
+}
+
+holaMundo();
